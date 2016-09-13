d30d5b304ed0de728d8ce373a6b85e18047ecb43
Iván Mera <ivan@cajanegra.com.ec>
2016-09-13 10:38:56 -0500

    Añadiendo ruta para registro de usuarios y para recurso profesores

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   resources :teachers
   root 'static_pages#home'
-  get  '/signup',    to: 'users#new'
-  resources :users
+  get  '/signup',    to: 'teachers#new'
   
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
