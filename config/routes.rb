98a5517091bfd6d75c979fb5628d3640a5f76547
Iván Mera <ivan@cajanegra.com.ec>
2016-09-14 12:24:37 -0500

    Creando recurso para quizzes

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,5 @@ Rails.application.routes.draw do
   post	 '/login',	   to: 'sessions#create'
   delete '/logout',    to: 'sessions#destroy'
   resources :teachers
-  
-  
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :quizzes
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160914123016) do
+ActiveRecord::Schema.define(version: 20160914171704) do
+
+  create_table "quizzes", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "teachers", force: :cascade do |t|
     t.string   "email"
