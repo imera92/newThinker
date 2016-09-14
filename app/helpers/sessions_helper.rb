1e39bb6188b73aee6874102d6b138ea5821a6d20
Iván Mera <ivan@cajanegra.com.ec>
2016-09-14 08:12:08 -0500

    Correcciones en implementacion de login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,11 +3,16 @@ module SessionsHelper
     	session[:teacher_id] = teacher.id
   	end
 
-  	def current_user
-    	@current_user ||= User.find_by(id: session[:user_id])
+  	def log_out
+	    session.delete(:teacher_id)
+	    @current_teacher = nil
+	  end
+
+  	def current_teacher
+    	@current_teacher ||= Teacher.find_by(id: session[:teacher_id])
   	end
 
   	def logged_in?
-		!current_user.nil?
+		!current_teacher.nil?
 	end
 end
