a260b31e06455fa40b89a9b6f79ae99c14cbfa9a
Iván Mera <ivan@cajanegra.com.ec>
2016-09-14 14:56:18 -0500

    Recreando recurso para quizzes

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160914191923) do
+ActiveRecord::Schema.define(version: 20160914195040) do
 
   create_table "quizzes", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
-    t.integer  "user_id"
+    t.integer  "teacher_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.index ["user_id"], name: "index_quizzes_on_user_id"
+    t.index ["teacher_id"], name: "index_quizzes_on_teacher_id"
   end
 
   create_table "teachers", force: :cascade do |t|
