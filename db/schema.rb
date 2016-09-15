59f11aea34f55e5b99c41a4cfb84064c8aa629b3
Iván Mera <ivan@cajanegra.com.ec>
2016-09-14 23:04:26 -0500

    Anadiendo modelo para preguntas

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160914201332) do
+ActiveRecord::Schema.define(version: 20160915040131) do
+
+  create_table "questions", force: :cascade do |t|
+    t.string   "title"
+    t.integer  "quiz_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "quizzes", force: :cascade do |t|
     t.string   "name"
